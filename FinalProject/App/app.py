f97dd188d45884993f6f3e7f40d30060bd966fe7
Jacopo Stringara <62561822+jstringara@users.noreply.github.com>
2023-06-16 01:25:30 +0200

    Fuffaggine

--- FinalProject/App/app.py
+++ FinalProject/App/app.py
@@ -21,18 +21,99 @@ st.set_page_config(
 st.title("PortFolio replication")
 st.write(
 """This is a tool to explore your portfolio replication options using
-Futures available on the NSE. \n
+Futures available on the market. \n
 If you want more information about the tool, please visit our [GitHub](
 https://github.com/jstringara/Fintech) page."""
 )
 
-#> Futures Selection <#
+#> Futures Presentation <#
+st.write("---")
+st.write("## Futures")
+st.write(
+"""Here is a rundown of the futures that we used in our study:
+- **RX1**: Fixed-income security issued by the Federal Republic of Germany.
+It is considered a benchmark bond and is widely regarded as one of the safest
+and most liquid government bonds. It's highly liquid, supported by active
+trading, high trading volumes, and the presence of market makers.
+- **CO1**: Price of Brent crude oil in the financial markets. It's a benchmark
+for global oil prices. The price of Brent crude oil can be volatile due to
+various factors.
+- **DU1**: The German 2-year government bond, known as the "Schatz." It has a
+maturity of 2 years. It's considered to be a Safe Haven asset and it is a
+relatively liquid instrument.
+- **ES1**: It represents a broad-based stock market index of 500 large companies
+listed on U.S. stock exchanges. The S&P 500 provides diversification by
+including a large number of stocks from different sectors and industries. This
+diversification helps spread risk across multiple companies and industries.
+- **GC1**: Price of gold. Gold is widely recognized as a store of value. It's a
+safe haven asset, particularly during times of economic uncertainty or market
+volatility. Investors tend to turn to gold as a hedge against inflation,
+currency fluctuations, and geopolitical risks. Gold can have sharp price
+movements in response to global events and market conditions. Gold is frequently
+used as a diversification tool in investment portfolios. Its low correlation
+with other asset classes, such as stocks and bonds, can help reduce overall
+portfolio risk.
+- **NQ1**: The Nasdaq 100 index. The Nasdaq 100 is an index that primarily
+focuses on the technology sector. It includes a wide range of technology-related
+companies. The Nasdaq 100 includes 100 of the largest non-financial companies
+listed on the Nasdaq stock exchange. These companies are typically
+well-established and have significant market capitalization.
+- **TP1**: It's associated with the Topix index. The Topix index represents the
+overall performance of the Japanese equity market. It is a broad-based stock
+market index that includes a wide range of companies listed on the Tokyo Stock
+Exchange (TSE).The Topix provides diversification by including companies from
+various sectors and industries within the Japanese equity market. Investing in
+the Topix provides exposure to the Japanese economy, which is one of the largest
+economies globally.
+- **TU2**: It refers to the 2-year US Treasury bond. The 2-year US Treasury bond
+is a debt security issued by the United States government to finance its
+operations and obligations. It is backed by the full faith and credit of the US
+government, making it a relatively low-risk investment. It is a fixed income
+instrument. US Treasury bonds are considered to be virtually risk-free due to
+the creditworthiness of the US government. They are often used as a benchmark
+for risk-free interest rates in financial markets.
+- **TY1**: 10-years US Treasury bond, it's a debt security issued by the United
+States government to finance its operations and obligations. It is backed by the
+full faith and credit of the US government, making it a relatively low-risk
+investment. US Treasury bonds are considered to be virtually risk-free due to
+the creditworthiness of the US government. They are often used as a benchmark
+for risk-free interest rates in financial markets. The US government actively
+supports the liquidity of its debt securities, ensuring there is a robust market
+for investors to transact. It's a Safe Haven Asset. 
+- **VG1**: Euro Stoxx 50 index. The Euro Stoxx 50 index represents the
+performance of the 50 largest and most liquid stocks across 12 Eurozone
+countries. It is a widely recognized benchmark for the European equity market.
+The Euro Stoxx 50 includes blue-chip companies that are considered leaders in
+their respective industries. These companies are typically well-established,
+large-cap entities with significant market capitalization. Market conditions and
+factors specific to the European economy can contribute to fluctuations in the
+index's performance."""
+)
 
+#> Futures Selection <#
 st.write("---")
 st.write("## Futures Selection")
 st.write(
-"""Please, start by selecting the type of approach you want to use to replicate
-your portfolio. \n"""
+"""For your convenience, we have pre-selected three groups of futures for you to
+tinker with.
+- **Base Futures**: This group contains all the futures that we used in our
+study and it will give you the most diversified portfolio. Furthermore, this
+comprehensive approach will enable you to capitalize on opportunities in
+different sector and geographic regions. You certainly can't go wrong with this.
+- **Sure-Fire Futures**: This group contains the futures that we found to have
+the lowest volatility. It is comprized of RX1, DU1, GC1, TU2, VG1 and ES1. This
+selection of government bonds, gold and equities from different regions provides
+a strong foundation for risk management and potential returns. Moreover, the
+portfolio benefits from the lilquidity of the selected futures, allowing for
+efficient rebalancing. We recommend this portfolio if you are looking for a
+stable, low-risk portfolio.
+- **Dark Horse Futures**: This group contains the futures that we found to have
+the highest volatility. It is comprized of ES1, NQ1, TP1, GC1, CO1, VG1, TY1 and
+RX1. This selection of equities from Japan, the US and the EU and Gold with just
+a touch of short-term bonds provides exposure to major American, Asian and EU
+markets as well as safe haven assets such as gold and short-term bonds. This
+selection combines the potential for high returns with the potential for high
+volatility. It is a high-risk, high-reward portfolio. Use at your own risk."""
 )
 
 # load the data
@@ -52,7 +133,9 @@ wHFRXGL = 0.5
 wMXWO = 0.25
 wLEGATRUU = 0.25
 
-y = wHFRXGL*price2ret(data.HFRXGL) + wMXWO*price2ret(data.MXWO) + wLEGATRUU*price2ret(data.LEGATRUU)
+y = wHFRXGL*price2ret(data.HFRXGL) + \
+    wMXWO*price2ret(data.MXWO) + \
+    wLEGATRUU*price2ret(data.LEGATRUU)
 target = (ret2price(y))
 target = target.dropna()
 
